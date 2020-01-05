8d6b694588f2da7561f5ec82b826a1788706dce0
Ajit <ajits01@outlook.com>
2020-01-05 19:05:55 +0530

    add Error Boundary to Details route

--- src/Details.js
+++ src/Details.js
@@ -1,31 +1,35 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends React.Component {
-  /* constructor(props) {
-    super(props);
-    this.state = {
-      loading: true
-    };
-  } */
-  state = { loading: true }; // can be used by enabling classProperties feature in babel config
+  state = { loading: true, redirect: false, hasErrors: false };
 
   componentDidMount() {
-    pet.animal(+this.props.id).then(({ animal }) => {
-      this.setState({
-        name: animal.name,
-        animal: animal.type,
-        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
-        description: animal.description,
-        media: animal.photos,
-        breed: animal.breeds.primary,
-        loading: false
-      });
-    }, console.error);
+    pet.animal(this.props.id).then(({ animal }) => {
+      if (animal) {
+        this.setState({
+          name: animal.name,
+          animal: animal.type,
+          location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+          description: animal.description,
+          media: animal.photos,
+          breed: animal.breeds.primary,
+          loading: false
+        });
+      } else {
+        this.setState({
+          hasErrors: true
+        });
+      }
+    });
   }
 
   render() {
+    if (this.state.hasErrors) {
+      throw new Error("Unable to load Pet Data");
+    }
     if (this.state.loading) {
       return <h1>Loading...</h1>;
     }
@@ -45,4 +49,10 @@ class Details extends React.Component {
   }
 }
 
-export default Details;
+export default function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
