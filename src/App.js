47372d3d383aa32ffdd61268f4ad6877c799ab4e
Ajit <ajits01@outlook.com>
2020-01-12 18:42:19 +0530

    code-splitting with lazy ans Suspense

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
-import { Router, Link } from "@reach/router";
+import React, { useState, lazy, Suspense } from "react";
+import { Router } from "@reach/router";
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
-import Details from "./Details";
 import ThemeContext from "./ThemeContext";
 import ThemeSelector from "./ThemeSelector";
 import NavBar from "./NavBar";
+
+const Details = lazy(() => import("./Details"));
 
 const App = () => {
   const themeHook = useState("peru");
@@ -15,10 +16,12 @@ const App = () => {
         <div id="app-container">
           <NavBar />
           <ThemeSelector />
-          <Router>
-            <SearchParams path="/" />
-            <Details path="/details/:id" />
-          </Router>
+          <Suspense fallback={<h1>Loading Route…</h1>}>
+            <Router>
+              <SearchParams path="/" />
+              <Details path="/details/:id" />
+            </Router>
+          </Suspense>
         </div>
       </ThemeContext.Provider>
     </React.StrictMode>
